82fbcd8b3ed34da8b27ff8947399f3a6ac894e5b
Ivan Miranda <pa.ivan.miranda@gmail.com>
2015-11-10 01:26:46 -0600

    Correccion utilidades

--- App/Core/Controladores/Reportes.php
+++ App/Core/Controladores/Reportes.php
@@ -19,7 +19,7 @@ class Controladores_Reportes extends Sfphp_Controlador
 		$data = Sfphp_Peticion::get('_parametros');
 		if($data['accion'] == 'csv') {
 			$data = $this->modeloReportes->utilidades($data['desde'],$data['hasta']);
-			$salida = array("Fecha,Venta,Gastos,Costos,Utilidad");
+			$salida = array("Fecha,Vendedor,Venta,Gastos,Costos,Utilidad");
 			foreach ($data as $registro) {
 				$utilidad = doubleval($registro['venta'])-doubleval($registro['gasto'])-doubleval($registro['costo']);
 				array_push($salida, implode(",",$registro).",".$utilidad);
--- App/Core/Modelos/Reportes.php
+++ App/Core/Modelos/Reportes.php
@@ -7,15 +7,19 @@ class Modelos_Reportes extends Sfphp_Modelo
 
 	public function utilidades($desde, $hasta)
 	{
-		$query = "SELECT vta.fecha, SUM(pag.monto) venta, SUM(gto.monto) gasto, SUM(pro.costo) costo
+		$query = "SELECT vta.fecha, usr.nombre, SUM(det.subtotal) venta, MAX(gto.monto) gasto, SUM(pro.costo*det.cantidad) costo
 		FROM ventas vta
-		INNER JOIN ventasPagos pag USING(venta)
 		INNER JOIN ventasProductos det USING(venta)
+		INNER JOIN vendedores ven USING(vendedor)
+		INNER JOIN usuarios usr USING(usuario)
 		INNER JOIN productos pro USING(producto)
-		INNER JOIN gastosRuta rta ON(rta.fecha = vta.fecha)
-		INNER JOIN gastosRutasDetalle gto USING(gastoRuta)
+		LEFT JOIN (
+			SELECT rta.fecha, rta.vendedor, SUM(gto.monto) monto FROM gastosRuta rta
+			INNER JOIN gastosRutasDetalle gto USING(gastoRuta)
+            GROUP BY rta.fecha, rta.vendedor
+        ) gto ON(gto.fecha = vta.fecha AND gto.vendedor = vta.vendedor)
 		WHERE vta.fecha between '{$desde}' AND '{$hasta}'
-		GROUP BY vta.fecha;";
+		GROUP BY vta.fecha, usr.nombre;";
 		return $this->db->query($query);
 	}
 }
